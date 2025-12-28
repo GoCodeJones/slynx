3e500d772eb6cf9a128140e9d99abf48f8149be0
cykna <kina.lima1302@proton.me>
2025-12-28 20:24:39 -0300

    feat: added support for compiling objects

--- src/compiler/js/mod.rs
+++ src/compiler/js/mod.rs
@@ -99,6 +99,7 @@ impl SlynxCompiler for WebCompiler {
         ir: &IntermediateRepr,
     ) -> Self::ExpressionType {
         match expr {
+            IntermediateExpr::Identifier(i) => Expr::Ident(create_ident("example")),
             IntermediateExpr::Int(int) => Expr::Lit(Lit::Num(Number {
                 span: DUMMY_SP,
                 value: *int as f64,
@@ -142,12 +143,14 @@ impl SlynxCompiler for WebCompiler {
                     type_args: None,
                 })
             }
+            IntermediateExpr::Struct { id, exprs } => self.compile_struct(id, exprs, ctx, ir),
             un => unimplemented!("{un:?}"),
         }
     }
 
     ///The flattener has everything it's required
     fn compile(mut self, ir: IntermediateRepr) -> Vec<u8> {
+        println!("{ir:#?}");
         for ctx in ir.contexts.iter() {
             self.hoist_ctx(ctx);
         }
